f140c0fc83b4037731674426eb455b2e88007914
jihyeon-e <jhw02040@gmail.com>
2021-08-01 23:16:11 +0900

    엔티티 매핑 예제

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.awt.*;
 import java.util.List;
 
 public class JpaMain {
@@ -17,16 +18,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            //Member findMember = em.find(Member.class, 1L);
-            //findMember.setName("HelloJPA");
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
+            Member member = new Member();
+            member.setUsername("C");
 
-            for (Member member : result) {
-                System.out.println("member = " + member.getName());
-            }
+            em.persist(member);
 
             tx.commit();
         } catch (Exception e) {
